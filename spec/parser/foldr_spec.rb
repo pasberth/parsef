f36bbf460a75fc82e0627c3802482aed2585c3a1
pasberth <pasberth@gmail.com>
2013-08-01 02:02:42 +0900

    foldr に関するテスト追加。

---
+++ spec/parser/foldr_spec.rb
@@ -0,0 +1,55 @@
+describe "foldr" do
+
+  example do
+    expect(`foldr cat -E <<< "a"`).to eq "a\$\n"
+  end
+
+  example do
+    expect(`foldr cat -E <<< "a\nb"`).to eq "a\$\nb\$\n"
+  end
+
+  example do
+    expect(`foldr cat -E <<< "a\nb\nc"`).to eq "a\$\nb\$\nc\$\n"
+  end
+
+  example do
+    expect(`foldr -@b cat -E <<< "a"`).to eq <<-EOF
+a$
+    EOF
+  end
+
+  example do
+    expect(`foldr -@b cat -E <<< "a\nb"`).to eq <<-EOF
+a$
+b$
+    EOF
+  end
+
+  example do
+    expect(`foldr -@b cat -E <<< "a\nb\nc"`).to eq <<-EOF
+a$
+b$
+c$$
+    EOF
+  end
+
+  example do
+    expect(`foldr -@b cat -E <<< "a\nb\nc\nb\nd"`).to eq <<-EOF
+a$
+b$
+c$$
+b$$
+d$$$
+    EOF
+  end
+
+  example do
+    expect(`foldr -@b -@d cat -E <<< "a\nb\nc\nd\ne"`).to eq <<-EOF
+a$
+b$
+c$$
+d$$
+e$$$
+    EOF
+  end
+end
