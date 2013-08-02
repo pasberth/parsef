9d0d718a8ab983b6693826571cb68da3f27f3a9c
pasberth <pasberth@gmail.com>
2013-08-03 03:03:52 +0900

    between 改良。テストは明日書く。

--- parser/between.zsh
+++ parser/between.zsh
@@ -1,22 +1,29 @@
 #!/usr/bin/env zsh
 open=$1; shift
 close=$1; shift
-comb=$1; shift
 
 accum=""
 stack=()
 
 while read L ; do
-  if [[ $L = $open ]] ; then
+  if [[ $#stack != 0 ]] && [[ $L = $close ]] ; then
+    resul=$(echo -n "$accum" | "$@")
+    accum="$stack[1]$resul\n"
+    shift stack
+  elif [[ $L = $open ]] ; then
     stack=("$accum" "$stack[@]")
     accum=""
   elif [[ $L = $close ]] ; then
-    resul=$(echo "$accum" | $comb "$@")
-    accum="$stack[1]$resul\n"
-    shift stack
+    echo "$(basename $0) '$open' '$close'" "$@"": \`$close' unexpected" >&2
+    exit 1
   else
     accum="$accum$L\n"
   fi
 done
 
-echo -n "$accum"
+if [[ $#stack != 0 ]] ; then
+  echo "$(basename $0) '$open' '$close'" "$@"": \`$close' expected" >&2
+  exit 1
+else
+  echo -n "$accum"
+fi
